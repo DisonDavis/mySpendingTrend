217ee38956b0ad3830dca698ea595f513af7059c
disondavis <disondavis@msn.com>
2023-01-04 10:25:23 +0530

    some more changes for testing

--- src/contexts/Transactions.js
+++ src/contexts/Transactions.js
@@ -1,25 +1,22 @@
-import React, { useContext, useState } from "react"
+import React, { useContext, useState } from 'react'
 
 const TransactionContext = React.createContext()
 
-
 export const TransactionConsumer = () => {
-    return useContext(TransactionContext)
+  return useContext(TransactionContext)
 }
+//some changes
 
 export const TransactionProvider = ({ children }) => {
-    const [transactions, setTransactions] = useState([])
+  const [transactions, setTransactions] = useState([])
 
-    const addNewTransaction = (newSpending) => {
-        setTransactions(arr => [...arr, newSpending])
-      
-    }
+  const addNewTransaction = (newSpending) => {
+    setTransactions((arr) => [...arr, newSpending])
+  }
 
-    return (
-        <TransactionContext.Provider value={{ transactions, addNewTransaction }}>
-
-            {children}
-
-        </TransactionContext.Provider>
-    )
-} 
+  return (
+    <TransactionContext.Provider value={{ transactions, addNewTransaction }}>
+      {children}
+    </TransactionContext.Provider>
+  )
+}
